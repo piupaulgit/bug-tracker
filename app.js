cdad929e98a8209cc2b29907ca10fb042b12ac58
themeplify <futurefocus2k17@gmail.com>
2018-11-13 07:57:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+class Person{
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+    }
+}
+
+let person1 = new Person('ffff', 'Sonai');
+console.log(`new object id id ${person1.id} and name is ${person1.name}`)
